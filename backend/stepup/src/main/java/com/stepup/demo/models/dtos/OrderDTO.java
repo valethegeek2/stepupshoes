4464a7e28f2b61a21e094147cc09831f0c780266
Con <ice21390222@uniwa.gr>
2026-03-20 03:51:35 +0200

    creating orderdto

--- backend/stepup/src/main/java/com/stepup/demo/models/dtos/OrderDTO.java
+++ backend/stepup/src/main/java/com/stepup/demo/models/dtos/OrderDTO.java
@@ -1,1 +1,33 @@
-//Na to ftiaxo
+package com.stepup.demo.models.dtos;
+
+import com.stepup.demo.models.Order;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+    private Long orderId;
+    private Long userId;
+    private Instant orderDate;
+    @NotNull
+    private BigDecimal totalAmount;
+    @NotNull
+    private Order.OrderStatus status;
+    @NotBlank
+    private String shippingAddress;
+    private String shippingCity;
+    private String shippingPostalCode;
+    @NotNull
+    private Order.PaymentMethod paymentMethod;
+    private Order.PaymentStatus paymentStatus;
+    private String notes;
+    private Instant updatedAt;
+}
